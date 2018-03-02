2721cd8b4f1967626b3911239a781c4adf220c95
DanWilliams24 <01danielwilliams@gmail.com>
2018-03-02 16:01:35 -0500

    Created the IOS course fetcher feature

---
+++ MYPApp/Course.swift
@@ -0,0 +1,14 @@
+//
+//  Course.swift
+//  MYPApp
+//
+//  Created by Daniel on 3/2/18.
+//  Copyright © 2018 Daniel Williams. All rights reserved.
+//
+
+import Foundation
+
+
+struct Course {
+    let name : String
+}
--- MYPApp/DataVC.swift
+++ MYPApp/DataVC.swift
@@ -10,33 +10,68 @@ import Foundation
 import UIKit
 import Alamofire
 import JSONJoy
-class DataVC: UIViewController {
+/*
+ A reoccuring issue when doing http requests has been the asynchronous processing that takes place in the background. This happens because the app prioritizes actions it can do immediately,while letting time consuming processes take the backseat. To work-around this, a completion handler is added to the end of the request to run once the request done. This completion handler then updates the table view with the updated data.
+ */
+
+class DataVC: UITableViewController {
+    var classes = [Course]()
+    var test = [Course]()
+    
+    
     
     override func viewDidLoad() {
-        Alamofire.request("http://localhost:3000/grades").responseJSON { response in
+        super.viewDidLoad()
+        test = [Course(name: "1"),Course(name: "2"),Course(name: "3"),Course(name: "4")]
+        let req = Alamofire.request("http://localhost:3000/grades").responseJSON { response in
             print("Request: \(String(describing: response.request))")   // original url request
             print("Response: \(String(describing: response.response))") // http url response
             print("Result: \(response.result)")                         // response serialization result
             
             if let json = response.result.value {
-               //print("JSON: \(json)") // serialized json response
                 do {
                     let sample = try Sample(JSONLoader(json))
-                    for name in sample.names {
-                        print(name)
+                    for course in sample.names {
+                        self.classes.append(Course(name: course))
+                        //print(self.classes[0])
                     }
                 }catch{
                     print("unable to parse the JSON")
+                    
                 }
-                
             }
+            return
+        }.response(completionHandler: {(response) in
+            self.tableView.reloadData()
+        })
+        req.resume()
         
-         
-        }
+        
         
     }
     
+    func dataHasLoaded() {
+        tableView.reloadData()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.classes.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = self.tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        var course : Course
+        course = classes[indexPath.row]
+        cell.textLabel?.text = course.name
+        return cell
+        
+    }
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
     }
     
     
